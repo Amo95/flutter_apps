24198c7981273fad565cbd79132727282f902201
amo95 <jameaamo@gmail.com>
2024-01-31 23:03:29 +0000

    added text and background

--- dice_game/lib/main.dart
+++ dice_game/lib/main.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       const MaterialApp(
         home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 61, 33, 123),
           body: Center(
-            child: Text("Hello World!"),
+            child: Text(
+              "Hello world!",
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
